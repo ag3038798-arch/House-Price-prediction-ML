a73318e6d9128cf09879158986f3c12ab547a5aa
ag3038798-arch <ag3038798@gmail.com>
2025-12-28 15:02:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,6 @@ import pandas as pd
 import time
 from sklearn.datasets import fetch_california_housing
 st.title('🏠House Price prediction using ML')
-st.image('https://www.colliers.com/en-in/news/press-release-housing-price-tracker-report-2022')
+st.image('https://i.pinimg.com/originals/f1/be/c8/f1bec81e20d80cd36c82379af920a4e9.gif')
+
 
