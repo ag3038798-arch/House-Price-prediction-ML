9ab6bddf6553de87f3a1bf5b8d3e3602ffe3dc14
ag3038798-arch <ag3038798@gmail.com>
2025-12-28 15:21:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,15 +6,16 @@ import time
 from sklearn.datasets import fetch_california_housing
 st.title('🏠House Price prediction using ML')
 st.image('https://i.pinimg.com/originals/f1/be/c8/f1bec81e20d80cd36c82379af920a4e9.gif')
-df =pd.read_csv('house_data.csv')
+df=pd.read_csv('house_data.csv')
 X = df.iloc[:,:-3]
 y = df.iloc[:,-1]
 
-st.sidebar.title('🏡 Select Jouse featurees')
+st.sidebar.title('🏡 Select Jouse features')
 st.sidebar.image('https://i.pinimg.com/originals/f1/be/c8/f1bec81e20d80cd36c82379af920a4e9.gif')
 all_value = []
 for i in X:
   ans = st.sidebar.slider(f'select {i} value')
   all_value.append(ans)
 st.write(all_value)
+
 
